9f05985f4ec019ccf6db62def2b1e44d471a5563
Myakot <cstepan@yandex.ru>
2024-07-25 13:51:47 +0700

    final improvements to the logger

--- FrogFight/main.py
+++ FrogFight/main.py
@@ -17,11 +17,10 @@ async def fight(frog1, frog2):
         frog1_damage = frog1.calculate_damage()
         frog2_armor = frog2.calculate_armor()
         frog1_final_damage = frog1_damage - frog2_armor
-        # print('DEBUG', frog1.calculate_damage(), frog2.calculate_armor())
-        # Tank armor works, dmg random and armor random works
         frog2_health -= frog1_final_damage
 
-        logger.debug(f"{frog1.__class__.__name__}1 наносит {frog1_damage} урона против {frog2_armor} брони")
+        logger.debug(f"{frog1.__class__.__name__}1 наносит {frog1_damage} урона против {frog2_armor} брони.\n"
+                     f"Здоровье {frog2.__class__.__name__}2: {frog2_health}")
 
         if frog2_health <= 0:
             logger.info(f"{frog1.__class__.__name__}1 победил в этой битве!")
@@ -32,7 +31,8 @@ async def fight(frog1, frog2):
         frog2_final_damage = frog2_damage - frog1_armor
         frog1_health -= frog2_final_damage
 
-        logger.debug(f"{frog2.__class__.__name__}2 наносит {frog2_damage} урона против {frog1_armor} брони")
+        logger.debug(f"{frog2.__class__.__name__}2 наносит {frog2_damage} урона против {frog1_armor} брони.\n"
+                     f"Здоровье {frog1.__class__.__name__}1: {frog1_health}\n")
 
         if frog1_health <= 0:
             logger.info(f"{frog2.__class__.__name__}2 победил в этой битве!")
